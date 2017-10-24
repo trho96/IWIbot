31a7f4bb16792ac0024c8cafb4d25dffbfe6350a
Luki <lukikugler@gmail.com>
2017-10-24 19:11:14 +0200

    fixed test urls

--- openwhisk/meal/test/meal_test.js
+++ openwhisk/meal/test/meal_test.js
@@ -2,7 +2,7 @@
  * Created by Armin on 05.06.2017.
  */
 var request = require('request');
-var actionUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/c9f88de3acb5a4648e4f118769d019c8df8797d1777c4342f43260626b4c51bf/iwibotTest/meal';
+var actionUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/8bcfac1b2e290d7c624a362c87384ca2a7e87ca8552f084a095f1fd8411d26e9/iwibotTest/meal';
 
 module.exports = {
     'Meal Action Test' : function (test) {
