68ef6e790d20c2be289bf4b047eeb6474a95beac
StefanFCMD <SHintzen@fc-md.umd.edu>
2018-02-10 18:03:55 +0100

    fixed typo

--- public/javascripts/conversation.js
+++ public/javascripts/conversation.js
@@ -3,7 +3,7 @@
 var exports = module.exports = {};
 var chat = require("./chat.js");
 var context = null;
-var url = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/' + $WSK_API_KEY + '/iwibot/router';
+var url = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/' + $WSK_API_CODE + '/iwibot/router';
 var $mainDiv = $("#mainDiv");
 var $btnCircle = $(".btn-circle");
 
