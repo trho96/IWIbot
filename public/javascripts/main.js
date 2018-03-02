d7796939e29c49151c9b08067bd0cadee559a911
Lukas Kugler <lukikugler@gmail.com>
2018-03-02 20:43:42 +0100

    map layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     
     var notificationNumber = 0;
 
-    var mymap = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([51.505, -0.09], 13);
     var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
