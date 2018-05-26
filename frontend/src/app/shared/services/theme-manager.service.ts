2be412c17d62bafbaec49b03a10386640975bb55
Niklas <nkreutzarek@hotmail.de>
2018-05-26 22:36:18 +0200

    Safe selected Theme

--- frontend/src/app/shared/services/theme-manager.service.ts
+++ frontend/src/app/shared/services/theme-manager.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +7,27 @@ export class ThemeManagerService {
 
   isDarkTheme: boolean = false;
 
-  constructor() { }
+  constructor() {
+    this.setThemeFromLocalStorage();
+    this.saveCurrentTheme();
+  }
 
   changeTheme() {
     this.isDarkTheme = !this.isDarkTheme;
   }
 
+  saveCurrentTheme() {
+    window.onbeforeunload = () => {
+      window.localStorage.setItem("isDarkTheme", String(this.isDarkTheme));
+    }
+  }
 
-
+  setThemeFromLocalStorage() {
+    const theme = window.localStorage.getItem("isDarkTheme");
+    if (theme) {
+      this.isDarkTheme = (theme == 'true');
+    }
+  };
 
 }
 
