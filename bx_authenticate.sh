f1f5cefa5c9a2e9006bbd867dc8b0d22cc382ed1
Niklas <nkreutzarek@hotmail.de>
2018-05-27 12:15:47 +0200

    Fix deployment of functions for travis

--- bx_authenticate.sh
+++ bx_authenticate.sh
@@ -6,7 +6,5 @@ bx login -a "$API_ENDPOINT" --apikey "$API_KEY" \
     -c "$BLUEMIX_ACCOUNT_ID"
 # target org in cf
 bx target --cf "$BLUEMIX_ORGANIZATION"
-#retrieve token
-bx iam oauth-tokens
 # install wsk plugin for the bluemix cli
 bx plugin install Cloud-Functions -r Bluemix
