c629b7480d80a520f2ccd7f56785d28712875075
Niklas <nkreutzarek@hotmail.de>
2018-06-01 02:15:04 +0200

    Add better display name for certificate link

--- openwhisk/bescheinigung/lib/Bescheinigung.js
+++ openwhisk/bescheinigung/lib/Bescheinigung.js
@@ -17,13 +17,14 @@ function main(params) {
                 if (!error && response.statusCode === 200) {
                     var bodyObject = JSON.parse(body);
                     bodyObject.forEach(function (element) {
-                        if (element.certificateType == params.entities[0].value.toLowerCase()) {
+                        if (element.certificateType === params.entities[0].value.toLowerCase()) {
                             pdfUrl += element.linkHashCode;
                         }
                     });
 
                     responseObject.payload = "Hier ist deine Bescheinigung:";
-                    responseObject.htmlText = "<a href='" + pdfUrl + "' target='_blank'>" + params.entities[0].value.toLowerCase() + "</a>";
+                    responseObject.htmlText = "<a href='" + pdfUrl + "' target='_blank'>" +
+                                              convertCertificateTypeToDisplayName(params.entities[0].value) + "</a>";
                     responseObject.language = language;
 
                     resolve(responseObject);
@@ -33,16 +34,37 @@ function main(params) {
                     switch (response.statusCode) {
                         case 500:
                             payload = "Der QIS-Server ist zurzeit offline.";
+                            break;
                         case 401:
-                            payload = "Um deine Bescheinigung zu erhalten, musst du dich einloggen."
+                            payload = "Um deine Bescheinigung zu erhalten, musst du dich einloggen.";
+                            break;
                     }
                     console.log('error:', error);
                     console.log('body:', body);
                     responseObject.payload = payload;
 
-                    reject(responseObject);
+                    resolve(responseObject);
                 }
             });
     });
+}
+function convertCertificateTypeToDisplayName(type) {
+    switch(type) {
+        case 'DATA_CONTROL_SHEET':
+            return 'Datenkontrollblatt';
+        case 'CERTIFICATE_OF_MATRICULATION':
+            return 'Immatrikulationsbescheinigung (deutsch)';
+        case 'CERTIFICATE_OF_MATRICULATION_ENGLISH' :
+            return 'Immatrikulationsbescheinigung (englisch)';
+        case 'BAFOEG':
+            return 'BAFÖG-Bescheinigung';
+        case 'KVV':
+            return 'KVV-Bescheinigung';
+        case 'DURATION_OF_STUDY':
+            return 'Studienzeitbescheinigung';
+        case 'IZ_ACCESS':
+            return 'Initiale IZ-Zugangsdaten';
+    }
+
 }
 exports.main = main;
