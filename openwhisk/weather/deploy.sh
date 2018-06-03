bfe20ad243bda367c7108c2cf1ea43e07233e53e
Niklas <nkreutzarek@hotmail.de>
2018-06-03 16:45:39 +0200

    Change line separator to LF

--- openwhisk/weather/deploy.sh
+++ openwhisk/weather/deploy.sh
@@ -11,5 +11,5 @@ rm -rf node_modules
 # recover dev deps
 mv .mod node_modules
 # install zip in openwhisk
-bx wsk action update Weather --kind nodejs:6 action.zip --web true
+bx wsk action update Weather --kind nodejs:6 action.zip --web 'true'
 bx wsk service bind weatherinsights Weather
